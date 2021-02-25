a402b2b1d2d17cead580efaece04f0a763c6a3a5
Madeleine Goertz <madiemoxie@outlook.com>
2021-02-25 01:30:56 +0000

    Implemented row_won method with class and refactored main.

--- main.py
+++ main.py
@@ -42,29 +42,78 @@ def make_move(board, player, col):
     col = get_move(board, player)
   return board
 
+ 
 # returns True if col is valid column
 def is_valid_col(col):
   return col > 0 and col <= N_COLS
 
-def has_won(board): # returns string of winning player or empty string, or "Tie"
-  return ""
+# returns true if the player passed in has won, false if not. 
+def has_won(board, player):
+  return row_won(board, player) or col_won(board, player) or left_diag_won(board, player)   or right_diag_won(board, player)
+
+# return True if that player has 4 in a row across. 
+# Looks like this
+# xxxx
+def row_won(board, player):
+  # Loop through rows (0 to 5)
+  has_won = False
+  r = 0
+  while r < N_ROWS and not has_won:
+    # Loop through cols (0 to 6)
+    c = 0
+    num_consecutive = 0
+    while c < N_COLS and not has_won:
+      if board[r][c] == player:
+        num_consecutive += 1
+        print("detected {} at r = {}, c = {}, n_c = {}".format(player, r, c, num_consecutive))
+      else:
+        num_consecutive = 0
+      has_won = num_consecutive >= 4
+      c += 1
+    r += 1
+  return has_won
+
+# x
+# x
+# x
+# x
+def col_won(board, player):
+  return False
+
+# x
+#  x
+#   x
+#    x
+def left_diag_won(board, player):
+  return False
+
+#    x
+#   x
+#  x
+# x
+def right_diag_won(board, player):
+  return False
+
 
 def main():
   player = "x"
   board = initialize()
-  winner = has_won(board)
+  has_won_game = False
+  turn = 1
 
-  while winner == "":
+  # main game loop elapses if someone wins or the board fills up
+  while not has_won_game and turn <= N_ROWS * N_COLS:
     print_board(board)
     col = get_move(board, player)
     board = make_move(board, player, col)
-    winner = has_won(board)
+    has_won_game = has_won(board, player)
     player = "o" if player == "x" else "x" # switch player
+    turn += 1
   
   print_board(board)
-  if winner == "Tie":
+  if not has_won_game: # if the game is over but no one won. 
     print("Tie game.")
   else:
-    print("Player {} wins!".format(winner))
+    print("Player {} wins!".format("o" if player == "x" else "x"))
 
 main()
